b2437f5a54b728750461f9d14538732027fa29bb
ColtenZimm <coltenz00@gmail.com>
2023-07-12 21:26:11 -0500

    finished route.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -53,3 +53,35 @@ router.put("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
+    if (!categoryData[0]) {
+      res.status(400).json({ message: "Category not found" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+  // update a category by its `id` value
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(400).json({ message: "Category not found" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+  // delete a category by its `id` value
+});
+
+module.exports = router;
